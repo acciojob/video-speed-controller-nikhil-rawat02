098901cca2d5da9ab6b1e7069b95fd6e1dca3143
Nikhil Rawat <117645790+nikhil-rawat02@users.noreply.github.com>
2023-05-21 22:28:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-const player = document.querySelector(".player");
-const video = player.querySelector(".viewer");
-const progress = player.querySelector(".progress");
-const progressBar = player.querySelector(".progress__filled");
-const toggle = player.querySelector(".toggle");
+const inputs = document.querySelectorAll('.controls input');
+const player = document.querySelector(".player")[0];
+const video = player.querySelector(".viewer")[0];
+const progress = player.querySelector(".progress")[0];
+const progressBar = player.querySelector(".progress__filled")[0];
+const toggle = player.querySelector(".toggle")[0];
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
 
+    function handleUpdate() {
+      const suffix = this.dataset.sizing || '';
+      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    }
 toggle.addEventListener("click", togglePlay);
 
+    inputs.forEach(input => input.addEventListener('change', handleUpdate));
+    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
 video.addEventListener("timeupdate", handlerProgress);
 
 for (let skip of skipButtons) {
